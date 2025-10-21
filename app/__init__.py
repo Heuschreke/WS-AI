db47b25cf76ca38d9a73b8af0c29b1dd02ed2394
Heuschreke <tigertimego2013@gmail.com>
2025-10-21 16:20:05 +0500

    Добавлена логика входа/выхода в систему

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from config import Config
-from app.routes import register_routes
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
-from app.models import models
+login = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -15,7 +15,10 @@ def create_app():
     
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
+    login.login_view = 'routes.login'
     
+    from app.routes import register_routes
     register_routes(app)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,54 +1,93 @@
-from flask import Blueprint, render_template, request, redirect, url_for, current_app, flash
-from werkzeug.utils import secure_filename
-from app.forms import LoginForm
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    current_app,
+    flash,
+)
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
 
+from app.models.models import User
+from werkzeug.utils import secure_filename
+from app.forms import LoginForm, RegistrationForm
+from urllib.parse import urlsplit
 import uuid
 import os
 
-bp = Blueprint('routes', __name__)
-
+bp = Blueprint("routes", __name__)
 
 
 @bp.route("/")
 @bp.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@bp.route("/login", methods=['GET', 'POST'])
+
+@bp.route("/login", methods=["GET", "POST"])
 def login():
+    db = current_app.extensions['sqlalchemy']
+    if current_user.is_authenticated:
+        return redirect(url_for("routes.home"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Добро пожаловать, {}! Запомнить меня={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('routes.home'))
-    # print(get_flashed_messages())
-    return render_template('login.html', form=form)
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.check_password(form.password.data):
+            # error_message = 'Неправильный логин или пароль'
+            flash("Неправильный логин или пароль")
+            return redirect(url_for("routes.login"))
+        login_user(user, remember=form.remember_me.data)
+        flash(
+            "Добро пожаловать, {}! Запомнить меня={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('routes.home')
+        return redirect(next_page)
+
+    return render_template("login.html", title="Sing In", form=form)
+
+
+@bp.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("routes.home"))
 
 @bp.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
+
 
 @bp.route("/gen")
+@login_required
 def gen():
-    return render_template('gen.html')
+    return render_template("gen.html")
 
-@bp.route('/upload', methods=['POST'])
+
+@bp.route("/upload", methods=["POST"])
 def upload():
-    if 'photo' not in request.files:
-        return redirect('/')
+    if "photo" not in request.files:
+        return redirect("/")
 
-    file = request.files['photo']
-    if file.filename == '':
-        return redirect('/')
+    file = request.files["photo"]
+    if file.filename == "":
+        return redirect("/")
 
-    if not file.filename.lower().endswith(('.jpg', '.png', '.jpeg')):
+    if not file.filename.lower().endswith((".jpg", ".png", ".jpeg")):
         return "Только изображения (JPG/PNG)!"
 
-    os.makedirs(current_app.config['UPLOAD_FOLDER'], exist_ok=True)
+    os.makedirs(current_app.config["UPLOAD_FOLDER"], exist_ok=True)
 
     filename = f"{uuid.uuid4()}_{secure_filename(file.filename)}"
-    file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-    return render_template('gen.html', uploaded_image=filename)
+    file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
+    return render_template("gen.html", uploaded_image=filename)
+
 
 def register_routes(app):
     app.register_blueprint(bp)
